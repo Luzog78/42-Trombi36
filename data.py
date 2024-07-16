25e84ddc6c130d8a483332fbe389a0f6a3f2c9fd
Luzog78 <luzog78@gmail.com>
2024-07-16 12:19:52 +0200

    ~ | URL fix

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ class Data:
 	KEY				= ''.join(chr(random.randint(32, 126)) for _ in range(100))
 
 	PORT			= 25577
-	URL				= f'http://luzog.fr:${PORT}/'
+	URL				= f'http://luzog.fr:{PORT}/'
 
 	TOKEN_UID		= ''
 	TOKEN_SECRET	= ''
